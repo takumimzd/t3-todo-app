5b56dc3bfdfdcfb1a84d88564b516d21c73bfa70
takumimzd <retnuh.takumi@gmail.com>
2023-06-03 22:48:53 +0900

    create CreateTodo component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
@@ -10,6 +11,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Toaster />
       <Component {...pageProps} />
     </SessionProvider>
   );
